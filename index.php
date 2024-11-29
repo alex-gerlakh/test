04b7dd0d2f3c97f9fc0c086021d14c7c62e7fe16
alex-gerlakh <oleksandr@ninesquares.studio>
2024-11-29 15:59:54 +0200

    dsfsdasda

---
+++ index.php
@@ -0,0 +1,15 @@
+<div class="sidebar-socials add-class"> <!-- Master Comment -->
+    <div class="socials-in">
+        <div class="socials-list class1 master-class">
+            <ul>
+                <li>List 1</li>
+                <li>List 2</li>
+                <li>List 3</li>
+                <li>List 4</li>
+                <li>List 5</li>
+                <li>List 6</li>
+                <li>List 7</li>
+            </ul>
+        </div>
+    </div>
+</div>
